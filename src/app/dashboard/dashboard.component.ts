c1d6884e236338cdb7b043b0761e826d5fada5fd
umar432 <umarkathap@Umars-MacBook-Pro.local>
2017-08-20 23:11:45 +0200

    Connection
    Connection established to DreamHub

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,21 +4,50 @@ declare var deepstream:any;
 @Injectable()
 export class DsService {
   get dsInstance() {
-    return deepstream('<YOUR APP URL>').login()
+    return deepstream('wss://154.deepstreamhub.com?apiKey=86ec8a85-5b1f-483f-9528-11eee5e6fbd2').login()
   }
 }
+export class templates{
+    name:string;
+}
+const TEMPLATES: templates[] = [
+     {'name': 'Single Col Template'},
+     {'name': 'Multi Col Template'}
+    
+];
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  templates = TEMPLATES;
+  idx = new Date;
+  id = this.idx.getDay() + this.idx.getDate() + this.idx.getTime();
+  name;
+  message;
+  template;
+  record;
+  constructor(private router:Router, private dsService: DsService) { }
 
   ngOnInit() {
+      
+    //   this.record.subscribe((val) => {
+    //   this.id; 
+    //   this.name = val.name;
+    //   this.message = val.message;
+    //   this.template = val.template;
+    // })
+    console.log(this.id);
   }
-  onCreate(){
+  onCreate(name, message, template){
+    this.record = this.dsService.dsInstance.record.getRecord('retro/'+this.id)
+    this.record.set('id',this.id);
+    this.record.set('name',name);
+    this.record.set('message',message);
+    this.record.set('template',template);
+    this.router.navigate(['/template',this.id]);
 
-  }s
+  }
 }
